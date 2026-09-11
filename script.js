e0f62c190b856e4afcf665f584b4df2b5eed624a
sebastianrotela100196-lab <sebastianrotela100196@gmail.com>
2026-09-11 18:58:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,26 +4,38 @@
 ===================================== */
 
 
-// Número WhatsApp AMERISSTORE
+// =========================
+// WHATSAPP
+// =========================
+
+
 const whatsapp = "595981841136";
 
 
-// Función para comprar productos
+
 function comprar(producto){
 
-    let mensaje = 
+
+    let mensaje =
+
     `Hola AMERISSTORE 👋%0A%0A` +
+
     `Estoy interesado en comprar:%0A` +
+
     `🎮 ${producto}%0A%0A` +
+
     `Quiero más información.`;
 
 
 
-    let url = 
+    let url =
+
     `https://wa.me/${whatsapp}?text=${mensaje}`;
 
 
-    window.open(url, "_blank");
+
+    window.open(url,"_blank");
+
 
 }
 
@@ -31,19 +43,41 @@ function comprar(producto){
 
 
 
-// Animación simple al cargar página
-
-document.addEventListener("DOMContentLoaded",()=>{
 
 
-    console.log(
-        "AMERISSTORE cargado correctamente 🚀"
-    );
+// =========================
+// CARGA PAGINA
+// =========================
 
-   const counter = document.getElementById("diamondCounter");
+
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
+
+
+
+console.log(
+"AMERISSTORE cargado correctamente 🚀"
+);
+
+
+
+
+
+
+// =========================
+// CONTADOR DIAMANTES
+// =========================
+
+
+const counter = 
+document.getElementById("diamondCounter");
+
+
 
 
 if(counter){
+
 
 
 let number = 0;
@@ -56,11 +90,13 @@ let target = 10000;
 let animation = setInterval(()=>{
 
 
-number += 250;
+
+number += 200;
 
 
 
 if(number >= target){
+
 
 
 number = target;
@@ -70,24 +106,154 @@ clearInterval(animation);
 
 
 
-document
-.querySelector(".diamond-stock")
-.classList.add("active");
+// activar brillo
+
+
+let box = 
+document.querySelector(".diamond-counter");
+
+
+
+if(box){
+
+box.classList.add("finished");
+
+}
+
+
+
+// lanzar diamantes una sola vez
+
+
+
+if(
+!sessionStorage.getItem(
+"diamondExplosion"
+)
+
+){
+
+
+crearDiamantes();
+
+
+sessionStorage.setItem(
+"diamondExplosion",
+"true"
+);
 
 
 }
 
 
 
-counter.innerHTML = number.toLocaleString();
+}
 
 
 
-},40);
+counter.innerHTML =
+"+" + number.toLocaleString();
+
+
+
+
+},50);
 
 
 
 }
+
+
+
+
 
 
 });
+
+
+
+
+
+
+
+
+
+// =========================
+// LLUVIA DE DIAMANTES
+// =========================
+
+
+function crearDiamantes(){
+
+
+
+const container = 
+document.createElement("div");
+
+
+
+container.className =
+"diamond-effect";
+
+
+
+document.body.appendChild(container);
+
+
+
+
+
+for(let i=0;i<80;i++){
+
+
+
+let diamond =
+document.createElement("div");
+
+
+
+diamond.className =
+"falling-diamond";
+
+
+
+diamond.innerHTML="💎";
+
+
+
+diamond.style.left =
+Math.random()*100+"%";
+
+
+
+diamond.style.animationDelay =
+Math.random()*1.5+"s";
+
+
+
+diamond.style.fontSize =
+(25 + Math.random()*35)+"px";
+
+
+
+container.appendChild(diamond);
+
+
+
+}
+
+
+
+
+setTimeout(()=>{
+
+
+container.remove();
+
+
+},4000);
+
+
+
+
+}
