6f662adbc6d3750982d6b12212771b0cc8398b2d
sebastianrotela100196-lab <sebastianrotela100196@gmail.com>
2026-09-11 15:42:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+/* =====================================
+   AMERISSTORE SCRIPT
+   Gaming | IA | Streaming | Digital
+===================================== */
 
+
+// Número WhatsApp AMERISSTORE
+const whatsapp = "595981841136";
+
+
+// Función para comprar productos
+function comprar(producto){
+
+    let mensaje = 
+    `Hola AMERISSTORE 👋%0A%0A` +
+    `Estoy interesado en comprar:%0A` +
+    `🎮 ${producto}%0A%0A` +
+    `Quiero más información.`;
+
+
+
+    let url = 
+    `https://wa.me/${whatsapp}?text=${mensaje}`;
+
+
+    window.open(url, "_blank");
+
+}
+
+
+
+
+
+// Animación simple al cargar página
+
+document.addEventListener("DOMContentLoaded",()=>{
+
+
+    console.log(
+        "AMERISSTORE cargado correctamente 🚀"
+    );
+
+
+});
